12d5ab2240f32b685188f5166fc67c598422c42f
scuri <scuri@86f90930-8b5f-43f8-b0d1-9726248d827e>
2015-04-23 16:50:51 +0000

    removed lua3 target

--- trunk/cd/src/make_uname.bat
+++ trunk/cd/src/make_uname.bat
@@ -7,7 +7,6 @@ call tecmake %1 "MF=cd_ftgl" %2 %3 %4 %5 %6 %7 %8
 call tecmake %1 "MF=cd_pdflib" %2 %3 %4 %5 %6 %7 %8
 
 call tecmake %1 %2 %3 %4 %5 %6 %7 %8
-REM call tecmake %1 "USE_GDK=Yes" %2 %3 %4 %5 %6 %7 %8
 
 call tecmake %1 "MF=cdpdf" %2 %3 %4 %5 %6 %7 %8
 call tecmake %1 "MF=cdgl" %2 %3 %4 %5 %6 %7 %8
@@ -33,17 +32,3 @@ call tecmake %1 "MF=cdcontextplus" %2 %3 %4 %5 %6
 call tecmake %1 "MF=cdluacontextplus5" "USE_LUA51=Yes" %2 %3 %4 %5 %6 %7 %8
 call tecmake %1 "MF=cdluacontextplus5" "USE_LUA52=Yes" %2 %3 %4 %5 %6 %7 %8
 call tecmake %1 "MF=cdluacontextplus5" "USE_LUA53=Yes" %2 %3 %4 %5 %6 %7 %8
-REM call tecmake %1 "MF=cdcairo" %2 %3 %4 %5 %6
-REM call tecmake %1 "MF=cdluacairo5" "USE_LUA51=Yes" %2 %3 %4 %5 %6 %7 %8
-REM call tecmake %1 "MF=cdluacairo5" "USE_LUA52=Yes" %2 %3 %4 %5 %6 %7 %8
-REM call tecmake %1 "MF=cdluacairo5" "USE_LUA53=Yes" %2 %3 %4 %5 %6 %7 %8
-
-if defined TECGRAF_INTERNAL goto tec_internal
-goto end
-
-:tec_internal
-call tecmake %1 "MF=cdlua3" %2 %3 %4 %5 %6 %7 %8
-call tecmake %1 "MF=cdluapdf3" %2 %3 %4 %5 %6 %7 %8
-call tecmake %1 "MF=cdluagl3" %2 %3 %4 %5 %6 %7 %8
-
-:end
